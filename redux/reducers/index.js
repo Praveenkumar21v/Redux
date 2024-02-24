229a16620314854890a436679a64c944c68211fa
Praveenkumar21v <151128238+Praveenkumar21v@users.noreply.github.com>
2024-02-24 21:56:05 +0530

    Delete redux directory

--- redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { counterReducer } from "./counterReducer";
-import { todoReducer } from "./todoReducer";
-
-export default combineReducers ({
-  counterReducer,
-   todoReducer
-});
